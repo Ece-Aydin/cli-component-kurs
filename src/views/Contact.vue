cefc2eeae9e2ad0492ee2189e63376a93d54b9e8
Simge Ece Aydın <simgece99@gmail.com>
2022-03-10 11:50:31 +0300

    vue-blog-prepared

--- src/views/Contact.vue
+++ src/views/Contact.vue
@@ -1,87 +1,98 @@
 <template>
-    
-        <h1>Contact With Us!</h1>
-        <div class="form-body">
-        <div class="row">
-            <p class="paragraph">Please fill out the form for your complaints, suggestions and requests.</p>
-                <div class="wrap-input1 validate-input" data-validate="Name is required">
-<input class="input1" type="text" name="name" placeholder="Name">
-<span class="shadow-input1"></span>
-</div>
-<div class="wrap-input1 validate-input" data-validate="Valid email is required">
-<input class="input1" type="text" name="email" placeholder="Email">
-<span class="shadow-input1"></span>
-</div>
-<div class="wrap-input1 validate-input" data-validate="Subject is required">
-<input class="input1" type="text" name="subject" placeholder="Subject">
-<span class="shadow-input1"></span>
-</div>
-<div class="wrap-input1 validate-input" data-validate="Message is required">
-<input class="input1" name="message" placeholder="Message" />
-<span class="shadow-input1"></span>
-</div>
-<div class="container-contact1-form-btn">
-<button class="contact1-form-btn" @click="contactForm">
-<span>  SEND </span>
-</button>
-        </div>
-        </div>
-        </div>
+  <h1>Contact With Us!</h1>
+  <div class="form-body">
+    <div class="row">
+      <p class="paragraph">
+        Please fill out the form for your complaints, suggestions and requests.
+      </p>
+      <div class="wrap-input1 validate-input" data-validate="Name is required">
+        <input class="input1" type="text" name="name" placeholder="Name" />
+        <span class="shadow-input1"></span>
+      </div>
+      <div
+        class="wrap-input1 validate-input"
+        data-validate="Valid email is required"
+      >
+        <input class="input1" type="text" name="email" placeholder="Email" />
+        <span class="shadow-input1"></span>
+      </div>
+      <div
+        class="wrap-input1 validate-input"
+        data-validate="Subject is required"
+      >
+        <input
+          class="input1"
+          type="text"
+          name="subject"
+          placeholder="Subject"
+        />
+        <span class="shadow-input1"></span>
+      </div>
+      <div
+        class="wrap-input1 validate-input"
+        data-validate="Message is required"
+      >
+        <input class="input1" name="message" placeholder="Message" />
+        <span class="shadow-input1"></span>
+      </div>
+      <div class="container-contact1-form-btn">
+        <button class="contact1-form-btn" @click="contactForm">
+          <span> SEND </span>
+        </button>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
-export default{
-    data() {
-        return{
-            formElements : ''
-        }
+export default {
+  data() {
+    return {
+      formElements: "",
+    };
+  },
+  methods: {
+    contactForm() {
+      this.formElements;
+      alert("Thanks! Your message has been delivered.");
     },
-    methods : {
-        contactForm() {
-            this.formElements;
-            alert("Thanks! Your message has been delivered.")
-
-        }
-    }
-}
+  },
+};
 </script>
 
 <style lang="scss">
-@mixin inputBlocks{
-    width: 35% - 10%;
-    background-color: #fff;
-    color: #330000;
-    font-family: 'Times New Roman', Times, serif;
-    margin-left: 500px;
-    margin-bottom: 10px;
-    border-color: #000000;
-    
+@mixin inputBlocks {
+  width: 35% - 10%;
+  background-color: #fff;
+  color: #330000;
+  font-family: "Times New Roman", Times, serif;
+  margin-left: 500px;
+  margin-bottom: 10px;
+  border-color: #000000;
 }
 
-.input1{
-    @include inputBlocks();
+.input1 {
+  @include inputBlocks();
 }
 
-@mixin send{
-    background-color: #660066;
-    color: bisque;
-    margin-left: 500px;
-    width: 50px;
-
+@mixin send {
+  background-color: #660066;
+  color: bisque;
+  margin-left: 500px;
+  width: 50px;
 }
-.contact1-form-btn{
-    @include send();
+.contact1-form-btn {
+  @include send();
 }
-h1{
-    color: #660066;
-    margin-left: 500px;
-}
-
-.paragraph{
-    color: #001f4d;
-    font-family: 'Times New Roman', Times, serif;
-    font-size: 20px;
-    margin-left: 500px;
+h1 {
+  color: #660066;
+  margin-left: 500px;
 }
 
+.paragraph {
+  color: #001f4d;
+  font-family: "Times New Roman", Times, serif;
+  font-size: 20px;
+  margin-left: 500px;
+}
 </style>
--- src/views/Divided.vue
+++ src/views/Divided.vue
@@ -1,109 +1,105 @@
 <template>
-    <div id="divided">
-        <h1>WELCOME TO MY-BLOG</h1>
-        <form class="LogIn" @submit.prevent v-if="showL">
-            <label class="input">Email:</label>
-            <input type="email" id="eM" class="input">
-            <br>
-            <label class="input">Password:</label>
-            <input type="password" id="pass" class="input">
-            <br>
-            <button @click="clickLog" class="butons">LOGIN</button>
-        </form>
+  <div id="divided">
+    <h1>WELCOME TO MY-BLOG</h1>
+    <form class="LogIn" @submit.prevent v-if="showL">
+      <label class="input">Email:</label>
+      <input type="email" id="eM" class="input" />
+      <br />
+      <label class="input">Password:</label>
+      <input type="password" id="pass" class="input" />
+      <br />
+      <button @click="clickLog" class="butons">LOGIN</button>
+    </form>
 
-        <form class="SignUp" @submit.prevent v-if="showS">
-            <label class="input">Email : </label>
-            <input type="email" id="Smail" class="input">
-            <br>
-            <label class="input">Name : </label>
-            <input type="text" id="Sname" class="input">
-            <br>
-            <label class="input">Password : </label>
-            <input type="password" id="Spassword" class="input" placeholder="en az 8 karakterli bir şifre belirleyiniz">
-            <br>
-            <button @click="clickSign" class="butons" >SIGN UP</button>
-        </form>
+    <form class="SignUp" @submit.prevent v-if="showS">
+      <label class="input">Email : </label>
+      <input type="email" id="Smail" class="input" />
+      <br />
+      <label class="input">Name : </label>
+      <input type="text" id="Sname" class="input" />
+      <br />
+      <label class="input">Password : </label>
+      <input
+        type="password"
+        id="Spassword"
+        class="input"
+        placeholder="en az 8 karakterli bir şifre belirleyiniz"
+      />
+      <br />
+      <button @click="clickSign" class="butons">SIGN UP</button>
+    </form>
 
+    <button @click="login" class="butons" v-if="!showL">Click for login</button>
+    <button @click="register" class="butons" v-if="!showS">
+      Click for register
+    </button>
 
-        <button @click="login" class="butons" v-if="!showL">Click for login</button>
-        <button @click="register" class="butons" v-if="!showS" >Click for register</button>
-       
-<p class="link"><a href="">Forgot your password? </a></p>
-
-    </div>
+    <p class="link"><a href="">Forgot your password? </a></p>
+  </div>
 </template>
 
 <script>
 export default {
-
-    data(){
-        return {
-            sendLoginForm : '',
-            sendRegisterForm : '',
-            showL : false,
-            showS : false,
-            
-        }
+  data() {
+    return {
+      sendLoginForm: "",
+      sendRegisterForm: "",
+      showL: false,
+      showS: false,
+    };
+  },
+  methods: {
+    login() {
+      this.sendLoginForm;
+      this.showL = true;
+      this.$emit("sendLog");
     },
-    methods : {
-        login() {
-            this.sendLoginForm;
-            this.showL = true;
-           this.$emit('sendLog');
-        
-        },
-        register() {
-            this.sendRegisterForm;
-            this.showS = true
-            this.$emit('sendReg');
-           
-        },
-   
-
-    }
-}
+    register() {
+      this.sendRegisterForm;
+      this.showS = true;
+      this.$emit("sendReg");
+    },
+  },
+};
 </script>
 
 <style lang="scss">
-
 @mixin butonButon {
-    background-color: #000000;
-    color: #fff;
-    margin: 25px;
-    margin-top: 50px;
+  background-color: #000000;
+  color: #fff;
+  margin: 25px;
+  margin-top: 50px;
 }
 
-.butons{
- @include butonButon();
+.butons {
+  @include butonButon();
 }
 
-@mixin inputBlocks{
-    width: 35% - 10%;
-    background-color: #fff;
-    color: #330000;
-    font-family: 'Times New Roman', Times, serif;
-    margin: 10px;
-    border-color: #000000;
-    margin-left: 500px;
-    
+@mixin inputBlocks {
+  width: 35% - 10%;
+  background-color: #fff;
+  color: #330000;
+  font-family: "Times New Roman", Times, serif;
+  margin: 10px;
+  border-color: #000000;
+  margin-left: 500px;
 }
-.input{
-    @include inputBlocks();
-}
-
-.heading{
-    text-align: center;
-    color: #ff66b3;
-    font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
-    
+.input {
+  @include inputBlocks();
 }
 
-a{
-    text-decoration: none;
+.heading {
+  text-align: center;
+  color: #ff66b3;
+  font-family: "Segoe UI", Tahoma, Geneva, Verdana, sans-serif;
 }
 
-.link{
-    margin-left: 25px;
-    margin-top: 25px;
+a {
+  text-decoration: none;
+}
+
+.link {
+  margin-left: 25px;
+  margin-top: 25px;
 }
 </style>
